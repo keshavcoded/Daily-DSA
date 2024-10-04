32b5dd1733ad5e463fb233ce8a8e342d93cbf2ef
spartadotts <keshav.shri06@gmail.com>
2024-10-04 11:23:16 +0530

    add: character hashing

--- hashing/CharacterHashing.java
+++ hashing/CharacterHashing.java
@@ -1,5 +1,23 @@
 package hashing;
 
+import java.util.Scanner;
+
 public class CharacterHashing {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        int hash[] = new int[26];
+        for(int i=0;i<s.length();i++){
+            hash[s.charAt(i) - 'a'] += 1;
+        }
+
+        int k = sc.nextInt();
+        while (k-->0) {
+            char c = sc.next().charAt(0);
+            System.out.println(hash[c-'a']);
+            
+        }
+        sc.close();
+    }
 }
