1abc2d8f00c5789b61da511fb7eab5ea3d7dcdbe
keshavcoded <keshav.shri06@gmail.com>
2025-02-04 10:01:31 +0530

    add: length of last word

---
+++ miscellaneous/LC58.java
@@ -0,0 +1,20 @@
+package miscellaneous;
+
+public class LC58 {
+
+    int lengthOfLastWord(String s) {
+        int end = s.length() - 1;
+
+        while (end >= 0 && s.charAt(end) == ' ') {
+            end--;
+        }
+
+        int start = end;
+        while (start >= 0 && s.charAt(start) != ' ') {
+            start--;
+        }
+
+        return end - start;
+    }
+
+}
